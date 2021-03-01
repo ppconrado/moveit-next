89fa8e35b0b02aba5c8b43dd5567495443eb86bc
ppconrado <ppconrado@yahoo.com.br>
2021-03-01 00:51:19 -0300

    (01/03) Fixed minor issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,5 @@
 import "../styles/global.css";
 
-import { ChallengesProvider } from "../contexts/ChallengesContext";
-import { CountdownProvider } from "../contexts/CountdownContext";
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
